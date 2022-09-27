4fcb979883fc44a34f9aa76a1a7b79f2278b029e
sheepla <62412884+sheepla@users.noreply.github.com>
2022-09-27 21:07:27 +0900

    Add main.go (just WIP)

--- main.go
+++ main.go
@@ -1,18 +1,80 @@
+//nolint
 package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"path"
 
 	"github.com/sheepla/ghin/gh"
+	"github.com/sheepla/ghin/ui"
 )
 
+// TODO: more elegant code
 func main() {
-	param := gh.NewSearchParam("vim")
-	result, err := gh.Search(param)
+	param := gh.NewSearchParam(os.Args[1])
+
+	repos, err := gh.Search(param)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
-	fmt.Println(result)
+	idx, err := ui.SelectRepo(*repos)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s/%s\n", (*repos)[idx].Owner, (*repos)[idx].Name)
+
+	releases, err := gh.GetReleases((*repos)[idx].Owner, (*repos)[idx].Name)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(releases)
+
+	idx, err = ui.SelectTag((*releases))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	assets := (*releases)[idx].Assets
+
+	idx, err = ui.SelectAsset(assets)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	url := assets[idx].DownloadURL
+	fmt.Println(url)
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	download(url, pwd)
+}
+
+func download(url string, dest string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	f, err := os.Create(path.Base(url))
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	io.Copy(f, res.Body)
+
+	return nil
 }
